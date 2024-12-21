f1c75ed7a5bff351d883b11fe0023386005b0acd
Ananya Pathak <pathakananya95@gmail.com>
2024-12-21 14:06:31 +0530

    sitemap, robot.txt

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 # Configure templates
 templates = Jinja2Templates(directory="app/templates")
 
+# Mount static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 @app.get("/")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/sitemap.xml")
+async def sitemap():
+    return FileResponse("app/static/sitemap.xml")
+
+@app.get("/robots.txt")
+async def robots():
+    return FileResponse("app/static/robots.txt")
 
 if __name__ == "__main__":
     import uvicorn
